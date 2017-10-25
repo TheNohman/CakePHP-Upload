4efba0e63a46b0d776e87079c6c5775215e1cd2a
Ludovic Huguenot <huguenot.ludovic@gmail.com>
2017-10-25 11:01:26 +0200

    First Commit
    Change read me,
    Update code to CakePHP3

--- Model/Behavior/UploadBehavior.php
+++ Model/Behavior/UploadBehavior.php
@@ -1,122 +1,97 @@
 <?php
-class UploadBehavior extends ModelBehavior{
 
-    /**
-    * Fields is used to define fields that are "uploadable"
-    * array(
-    *   'avatar' => 'img/:id'
-    * )
-    *
-    * :id     => Record ID
-    * :id1000 => ceil( Record ID / 1000 )
-    * :id100  => ceil( Record ID / 100 )
-    * :y      => year
-    * :m      => month
-    * :uid    => user id (Auth.User.id)
-    * :md5    => random MD5
-    **/
-    private $defaultOptions = array(
-        'fields' => array()
-    );
-    private $options = array();
+namespace Upload\Model\Behavior;
 
-    public function setup(Model $model, $config = array()){
-        $this->options[$model->alias] = array_merge($this->defaultOptions, $config);
+use Cake\ORM\Behavior;
+use Cake\ORM\Entity;
+use Cake\ORM\TableRegistry;
+use Cake\Event\Event;
+use Cake\Datasource\EntityInterface;
+use ArrayObject;
+use Cake\Utility\Inflector;
+
+class UploadBehavior extends Behavior {
+
+    public $defaultOptions = [
+        'fields' => []
+    ];
+    public $options;
+
+    public function initialize(array $config) {
+        $this->options = array_merge($this->defaultOptions, $config);
     }
 
-    /**
-    * CakePHP Model Functions
-    **/
-    public function afterSave(Model $model, $created, $options = array()){
-        $data = $model->data;
-        foreach($this->options[$model->alias]['fields'] as $field => $path){
-           if(
-                isset($data[$model->alias][$field . '_file']) &&
-                !empty($data[$model->alias][$field . '_file']['name']) &&
-                (
-                    !$model->whitelist ||
-                    empty($model->whitelist) ||
-                    in_array($field, $model->whitelist)
-                )
-            ){
-                $file = $data[$model->alias][$field . '_file'];
-                $extension = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
-                $path = $this->getUploadPath($model, $path, $extension);
-                $dirname = dirname($path);
-                if(!file_exists(WWW_ROOT . $dirname)){
-                    mkdir(WWW_ROOT . $dirname, 0777, true);
-                }
-                $model->deleteOldUpload($field);
-                $model->move_uploaded_file(
-                    $file['tmp_name'],
-                    WWW_ROOT . $path
-                );
-                chmod(WWW_ROOT . $path, 0777);
-                $model->saveField($field, '/' . $path);
-           }
-        }
+    public function afterSave(Event $event, EntityInterface $entity, ArrayObject $options) {
+        foreach ($this->options['fields'] as $field => $path):
+            if (isset($entity->toArray()[$field . '_file']) && !empty($entity->toArray()[$field . '_file']['name'])):
+                $file = $entity->toArray()[$field . '_file'];
+                $extention = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+                $destination = $this->getUploadPath($entity, $path, $extention, $file['name']);
+                $dirname = dirname($destination);
+                if (!file_exists(WWW_ROOT . $dirname)):
+                    mkdir(WWW_ROOT . $dirname, 0777, TRUE);
+                endif;
+                $this->deleteOldUpload($entity, $field);
+                move_uploaded_file($file['tmp_name'], $destination);
+                chmod(WWW_ROOT . $destination, 0777);
+                $this->saveField($entity, $field, $destination);
+            endif;
+        endforeach;
     }
 
-    public function beforeDelete(Model $model, $cascade = true){
-        foreach($this->options[$model->alias]['fields'] as $field => $path){
-            $model->deleteOldUpload($field);
-        }
-        return true;
+    public function beforeDelete(Event $event, EntityInterface $entity, ArrayObject $options) {
+        foreach ($this->options['fields'] as $field => $path):
+            $this->deleteOldUpload($entity, $field);
+        endforeach;
+        return TRUE;
     }
 
-    /**
-     * Alias for the move_uploaded_file function, so it can be mocked for testing purpose
-    */
-    public function move_uploaded_file(Model $model, $source, $destination){
-        move_uploaded_file($source, $destination);
+    public function fileExtentions($file, $extentions, $allowEmpty = TRUE) {
+        if ($allowEmpty && empty($file['tmp_name'])):
+            return TRUE;
+        endif;
+        $extention = strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+        return in_array($extention, $extentions);
     }
 
-    /**
-     * Custom Validation Rules
-     */
-    public function fileExtension(Model $model, $check, $extensions, $allowEmpty = true){
-        $file = current($check);
-        if($allowEmpty && empty($file['tmp_name'])){
-            return true;
-        }
-        $extension = strtolower(pathinfo($file['name'] , PATHINFO_EXTENSION));
-        return in_array($extension, $extensions);
+    public function maxSize($file, $size, $allowEmpty = TRUE) {
+        if ($allowEmpty && empty($file['tmp_name'])):
+            return TRUE;
+        endif;
+        return $file['size'] <= $size;
     }
 
-    /**
-    * MISC
-    **/
-    private function getUploadPath(Model $model, $path, $extension){
-        $path = trim($path, '/');
-        $replace = array(
-            ':id1000'  => ceil($model->id / 1000),
+    private function getUploadPath(Entity $entity, $path, $extention, $filename) {
+        $replace = [
+			':id1000'  => ceil($model->id / 1000),
             ':id100'   => ceil($model->id / 100),
-            ':id'      => $model->id,
-            ':y'       => date('Y'),
-            ':m'       => date('m'),
-            ':uid'     => CakeSession::read('Auth.User.id'),
-            ':md5'     => md5(rand() . uniqid() . time())
-        );
-        $path = strtr($path, $replace) . '.' . $extension;
-        return $path;
+            '%id' => $entity->id,
+            '%y' => date('Y'),
+            '%m' => date('m'),
+            '%n' => $entity->name,
+            '%s' => $entity->slug,
+            '%filename' => Inflector::slug(basename($filename, $extention))
+        ];
+        return trim(strtr($path, $replace), '/') . '.' . $extention;
     }
 
-    public function deleteOldUpload(Model $model, $field){
-        $file = $model->field($field);
-        if(empty($file)){
-            return true;
-        }
-        $info = pathinfo($file);
-        $subfiles = glob(WWW_ROOT . $info['dirname'] . DS . $info['filename'] . '_*x*.*');
-        if(file_exists(WWW_ROOT . $file)){
-            unlink(WWW_ROOT . $file);
-        }
-        if($subfiles){
-            foreach($subfiles as $file){
-                unlink($file);
-            }
-        }
+    private function saveField(Entity $entity, $field, $destination) {
+        $tables = TableRegistry::get($entity->source());
+        $query = $tables->query();
+        return $query->update()
+                        ->set([$field => $destination])
+                        ->where(['id' => $entity->id])
+                        ->execute();
     }
 
+    public function deleteOldUpload(Entity $entity, $field) {
+        $file = $entity->$field;
+        if (empty($file)):
+            return TRUE;
+        endif;
+        if (file_exists(WWW_ROOT . $file)):
+            return unlink(WWW_ROOT . $file);
+        endif;
+    }
 
 }
